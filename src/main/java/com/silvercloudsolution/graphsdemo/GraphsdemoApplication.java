62c8bfe0bb37ff742e65af36127325f15d18aa2b
Atharva <atharvasaswade11@gmail.com>
2022-07-30 09:54:45 +0530

    PDF module tested and added.

--- src/main/java/com/silvercloudsolution/graphsdemo/GraphsdemoApplication.java
+++ src/main/java/com/silvercloudsolution/graphsdemo/GraphsdemoApplication.java
@@ -1,20 +1,37 @@
 package com.silvercloudsolution.graphsdemo;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.swing.*;
+import java.io.IOException;
 
 @SpringBootApplication
 public class GraphsdemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(GraphsdemoApplication.class, args);
 
 		LineChartExample example = new LineChartExample("Line Chart Example");
 
-		}
+		PDDocument document = new PDDocument();
+		PDPage firstPage = new PDPage();
+		document.addPage(firstPage);
+
+		PDImageXObject image = PDImageXObject.createFromFile("D:\\graphsdemo\\src\\main\\java\\com\\silvercloudsolution\\graphsdemo\\charttest.jpeg",document);
+		PDPageContentStream contentStream = new PDPageContentStream(document, firstPage);
+		contentStream.drawImage(image,25,25,320, 212);
+		contentStream.close();
+
+		document.save("D:\\graphsdemo\\src\\main\\java\\com\\silvercloudsolution\\graphsdemo\\charttest.pdf");
+		document.close();
+		System.out.println("PDF Created");
 	}
+}
 
 
 
